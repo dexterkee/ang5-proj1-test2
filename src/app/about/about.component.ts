b0bbe415c8321dba44f2fd906a90e0438e9b8044
Orban Szilard <szilard.orban@gmail.com>
2017-11-12 01:35:15 +0200

    Initial checkin

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DataService } from '../data.service';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class AboutComponent implements OnInit {
+
+  anyText: string = '';
+  goals: any;
+
+  constructor(private route: ActivatedRoute, private router: Router, private _data: DataService) { 
+      this.route.params.subscribe(res => this.anyText = res.id);
+      // this.route.params.subscribe(res => this.displayRes(res));
+  }
+
+  ngOnInit() {
+      this._data.goal.subscribe(res => this.goals = res);
+  }
+
+  displayRes(res) {
+      console.log(res.id)
+  }
+
+  sendMeHome() {
+      this.router.navigate(['']);
+  }
+
+}
